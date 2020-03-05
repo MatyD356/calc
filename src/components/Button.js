3e112bec76a77fe3ff5104d73026a4974d9ed303
Mateusz <matyd356@gmail.com>
2020-03-05 13:04:45 +0100

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+class Button extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            input: null
+        }
+    }
+    render() {
+        return (
+            <button />
+        );
+    }
+}
