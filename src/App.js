d8a1b57a61673e9ae32fe35a61bafd44e091050b
Mateusz <matyd356@gmail.com>
2020-03-06 19:03:27 +0100

    before refactor commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,81 @@
 import React from 'react';
 import './App.css';
+import CalcButtons from './components/Button'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      input: null
+      activeInput: false,
+      activeArr: [],
+      input: [],
+      val: 0
     }
+  }
+
+  componentDidMount() {
+    let script = document.createElement('script')
+    script.src = 'https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js'
+    document.body.appendChild(script)
+  }
+
+  calc = () => {
+    this.setState({
+      input: this.state.input.concat(this.state.activeArr.join('')),
+      activeArr: []
+    }, () => {
+      this.state.input.map((item, index) => {
+        if (item === "+") {
+          this.setState({
+            activeArr: [parseInt(this.state.input[index - 1], 10) + parseInt(this.state.input[index + 1], 10)]
+          }, () => { console.log(this.state.val) })
+        }
+        return null;
+      })
+    });
+  }
+
+  getDataFromChild = (childData) => {
+    if (/\d/.test(childData) === true) {
+      this.setState({
+        activeInput: true,
+        activeArr: this.state.activeArr.concat(childData)
+      })
+    }
+    else if (childData === '.') {
+      this.setState({
+        activeArr: this.state.activeArr.concat(childData)
+      })
+
+    } else {
+      this.setState({
+        input: (this.state.input.concat(this.state.activeArr.join('')).concat(childData)),
+        activeArr: []
+      })
+    }
+  }
+
+  clear = () => {
+    this.setState({
+      activeInput: false,
+      input: [],
+      val: 0
+    })
   }
   render() {
     return (
       <div className="App">
-
-
+        <div id="display"
+          className="display"
+          onClick={this.calc}>
+          {this.state.activeInput === false ? 0 : this.state.input.length < 1 ? this.state.activeArr : this.state.input}</div>
+        <div
+          className="display">
+          {this.state.activeArr.length < 1 ? 0 : this.state.activeArr}</div>
+        <CalcButtons
+          parentCalc={this.calc}
+          clean={this.clear}
+          sendData={this.getDataFromChild} />
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,31 @@
-class Button extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            input: null
-        }
+import React from 'react';
+
+class CalcButtons extends React.Component {
+    addValue = (e) => {
+        this.props.sendData(e.target.innerHTML)
     }
     render() {
         return (
-            <button />
+            <div className="buttons-container">
+                <button id="clear" onClick={this.props.clean}>AC</button>
+                <button id="equals" onClick={this.props.parentCalc}>=</button>
+                <button id="add" onClick={this.addValue}>+</button>
+                <button id="subtract" onClick={this.addValue}>-</button>
+                <button id="divide" onClick={this.addValue}>/</button>
+                <button id="multiply" onClick={this.addValue}>*</button>
+                <button id="decimal" onClick={this.addValue}>.</button>
+                <button id="zero" onClick={this.addValue}>0</button>
+                <button id="one" onClick={this.addValue}>1</button>
+                <button id="two" onClick={this.addValue}>2</button>
+                <button id="three" onClick={this.addValue}>3</button>
+                <button id="four" onClick={this.addValue}>4</button>
+                <button id="five" onClick={this.addValue}>5</button>
+                <button id="six" onClick={this.addValue}>6</button>
+                <button id="seven" onClick={this.addValue}>7</button>
+                <button id="eight" onClick={this.addValue}>8</button>
+                <button id="nine" onClick={this.addValue}>9</button>
+            </div>
         );
     }
 }
+export default CalcButtons 
