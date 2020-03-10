7c645ae0f82508c6874c0d4d3594a2939f8b35d8
Mateusz <matyd356@gmail.com>
2020-03-10 11:49:44 +0100

    Updates

---
+++ precache-manifest.d43b22792504cb298e7d5cadd0fd0697.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da14822070f92873cfed6033e235bf3",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "c1fdda3b6da225ec7090",
+    "url": "/calc/static/css/main.a1160d39.chunk.css"
+  },
+  {
+    "revision": "f4a247f8869140c25b61",
+    "url": "/calc/static/js/2.4af51320.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/calc/static/js/2.4af51320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1fdda3b6da225ec7090",
+    "url": "/calc/static/js/main.26942f6a.chunk.js"
+  },
+  {
+    "revision": "cf440316f01df5d705bc",
+    "url": "/calc/static/js/runtime-main.b37eec64.js"
+  }
+]);
